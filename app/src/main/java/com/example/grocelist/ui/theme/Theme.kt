3cf4c99b443baa1ee2780f133664b7ae8845403a
joaopsramos <joaopsramos231@gmail.com>
2022-11-13 20:49:36 -0300

    update theme

--- app/src/main/java/com/example/grocelist/ui/theme/Theme.kt
+++ app/src/main/java/com/example/grocelist/ui/theme/Theme.kt
@@ -5,12 +5,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+//    primary = Purple200,
+//    primaryVariant = Purple700,
+//    secondary = Teal200,
+    primary = Primary,
     primaryVariant = Purple700,
-    secondary = Teal200,
+    secondary = Secondary,
+    background = Background
 )
 
 private val LightColorPalette = lightColors(
